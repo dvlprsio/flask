717a0cc156193b6734b7e0c117718dbb5f65da2c
KH <kh@dvlprs.io>
2017-07-08 23:35:09 +0100

    add type checking for age

--- index.py
+++ index.py
@@ -11,9 +11,10 @@ def index():
 def goodbye():
     return "Goodbye World!"
 
-@app.route("/hello/<name>")
-def hello_name(name):
-    return "Hello {}".format(name)
+@app.route("/hello/<name>/<int:age>")
+def hello_name(name,age):
+    return "Hello {}, your are {}".format(name,age)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
